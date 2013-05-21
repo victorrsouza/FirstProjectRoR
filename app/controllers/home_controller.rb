a472fd19bf43f0e604428cc36f983ca4d360b719
Victor Rios <victorrsouza@gmail.com>
2013-05-20 23:50:19 -0300

    commit de exemplo em rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+	@welcome = "Welcome everybody!"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoRoR::Application.config.secret_token = '643acda4b7f64bfd1fbac87cbad3ff6c9bacc09469d5ceacef24a5b8083734b3aa23857e0970441dbd4165bc6f772df5c92f3ff3946411f69757aae6b6bcc8f4'
